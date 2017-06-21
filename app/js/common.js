80f8bbddd16ac1c2341707c3f7e507cc9b618e8c
Vyacheslav Lamash <slava91billion@gmail.com>
2017-06-21 13:46:54 +0400

    new commit

---
+++ app/js/common.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+		
+// 				var progressbar = $('.progressbar'),
+// 						max = progressbar.attr('max'),
+// 						time = (1000/max)*50,	
+// 						value = progressbar.val();
+
+// 						var loading = function() {
+// 								value += 1;
+// 								addValue = progressbar.val(value);
+ 
+// 								$('.progress-value').html(value + '%');
+ 
+// 								if (value == value) {
+// 										clearInterval(animate);			           
+// 								}
+// 						};
+ 
+// 						var animate = setInterval(function() {
+// 								loading();
+// 						}, time);
+		
+// });
