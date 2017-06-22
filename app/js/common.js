f9e060c348c2ce3370dfdef5821301072746869e
Vyacheslav Lamash <slava91billion@gmail.com>
2017-06-22 16:54:14 +0400

    more adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,53 @@
-// $(document).ready(function() {
-		
-// 				var progressbar = $('.progressbar'),
-// 						max = progressbar.attr('max'),
-// 						time = (1000/max)*50,	
-// 						value = progressbar.val();
+window.onload = function () {
+		var width = 1;
+		var elem = document.getElementsByClassName("myBar");
+		var maxValue = document.getElementsByClassName('max-value'); 
 
-// 						var loading = function() {
-// 								value += 1;
-// 								addValue = progressbar.val(value);
- 
-// 								$('.progress-value').html(value + '%');
- 
-// 								if (value == value) {
-// 										clearInterval(animate);			           
-// 								}
-// 						};
- 
-// 						var animate = setInterval(function() {
-// 								loading();
-// 						}, time);
-		
-// });
+		for(var i = 0; i < elem.length; i++) {
+			var params = {
+				elem: elem[i],
+				maxElem: maxValue[i],
+				width: width,
+				interval: null
+			};
+			params.interval = setInterval(frame, 10, params);
+		}
+
+		function frame(aParams) {
+			if (aParams.width >= aParams.maxElem.dataset.max) {
+				clearInterval(aParams.interval);
+			} 
+			else {
+				aParams.width++;
+				aParams.elem.style.backgroundColor = '#fff';
+				aParams.elem.style.width = aParams.width + '%';
+				aParams.maxElem.innerHTML = aParams.width + '%';
+			}
+		}
+
+		var btn = document.getElementById('hamburg');
+		btn.onclick = function(){
+			var nav = document.getElementById('nav');
+			if(nav.style.display == 'none'){
+				nav.style.display = 'inline-block';
+			}
+			else{
+				nav.style.display = 'none';
+			}
+		}
+
+
+};
+
+// window.onload = function menu() {
+// 	var btn = document.getElementById('hamburg');
+// 	btn.onclick = function(){
+// 		var nav = document.getElementById('nav');
+// 		if(nav.style.display == 'none'){
+// 			nav.style.display = 'inline-block';
+// 		}
+// 		else{
+// 			nav.style.display = 'none';
+// 		}
+// 	}
+// }
